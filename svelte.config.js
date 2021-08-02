37ddef241e50ba9f50432efd40d6e874c36e99b8
OrangeBurrito <mali.kewl.pool@gmail.com>
2021-08-02 16:26:03 +0700

    add netlify  adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import staticAdapter from '@sveltejs/adapter-static';
 import nodeAdapter from '@sveltejs/adapter-node';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: nodeAdapter(),
-		// target: '#svelte'
+		adapter: netlifyAdapter(),
+		target: '#svelte'
 	}
 };
 
